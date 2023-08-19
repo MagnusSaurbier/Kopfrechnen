e222c5c86f6ee400ade026e67fe392013c5fffc9
Magnus Saurbier <magnus.saurbier@tum.de>
2023-08-19 16:22:44 +0200

    time Tracking

--- Kopfrechnen.py
+++ Kopfrechnen.py
@@ -14,6 +14,8 @@ answered = False # True if answer was checked, False if Task is shown and answer
 task = "" # task to be solved
 result = 0 # correct result of task
 gamemode = gamemodes["Multiplication"][0] # current gamemode
+taskStartTime = time.time() # time when task was generated
+timeFile = open("Kopfrechnen_time.txt", "a") # file to write time to
 
 # GUI elements
 Task = tk.Label(root, text="Task", font=("Arial", 40))
@@ -44,7 +46,7 @@ def setTaskType(g):
 
 def next():
     """Generate new task"""
-    global answered, task, result, gamemode
+    global answered, task, result, gamemode, taskStartTime
     task, result = gamemode.makeTask()
 
     Task.config(text=task)
@@ -54,29 +56,41 @@ def next():
     Next.config(state="disabled")
 
     answered = False # new task is to be solved
+    taskStartTime = time.time()
 
 def checkAnswer():
     """Check if answer is correct, show result in green or red"""
+    global answered, taskStartTime
+
     try: answer = int(Answer.get())
     except: pass
     Result.config(text=result)
     if result == answer:
         Result.config(fg="lightgreen")
+        correct = True
     else:
         Result.config(fg="red")
+        correct = False
     Next.config(state="normal")
 
-    global answered
     answered = True # answer was checked
+    timeDelta = time.time() - taskStartTime
+    print(f"{gamemode.title}, correct:{correct}, task:{task}, result:{result}, answer:{answer}, time:{timeDelta}")
+    timeFile.write(f"{gamemode.title}, correct:{correct}, task:{task}, result:{result}, answer:{answer}, time:{timeDelta}\n")
 
 def PressedEnter():
     """Callback for Enter key: Switches state between answered and not answered"""
-    print("click", answered)
     if not answered: checkAnswer()
     else: next()
+
+# Close timeFile on exit
+def on_closing():
+    timeFile.close()
+    root.destroy()
 
 Answer.bind("<Return>", lambda x: PressedEnter())
 Next.config(command=next)
 
 next()
+root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
